15c6bb33da364a4ce7a4689d0eb4aaef8df3dd07
replyrebuff <44467554+replyrebuff@users.noreply.github.com>
2023-02-10 12:57:27 +0100

    updated imports

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-import requests
-import threading
+from itertools import product
+from requests import get
+from threading import Thread
 
 BASE_URL = 'https://bannedwagon.team/codes/'
 LETTERS = ['Q', 'H', 'O', 'C', 'D', 'I', 'P']
@@ -10,19 +10,18 @@ FOUND = []
 
 def generate_combinations(letters) -> list[str]:
     combinations = []
-    for i in itertools.product(letters, repeat=CODE_LENGTH):
+    for i in product(letters, repeat=CODE_LENGTH):
         combinations.append(''.join(map(str, i)))
     return combinations
 
 
 def check_combination(combination: str) -> int:
-    response = requests.get(BASE_URL + combination)
+    response = get(BASE_URL + combination)
     print(f'{combination} - {response.status_code}')
     if response.status_code == 200:
         FOUND.append(combination)
         with (open('found.txt', 'a')) as f:
             f.write(combination + '\n')
-    return response.status_code
 
 
 def main():
@@ -30,15 +29,13 @@ def main():
     threads = []
 
     for combination in combinations:
-        thread = threading.Thread(
-            target=check_combination, args=(combination,))
+        thread = Thread(target=check_combination, args=(combination,))
         threads.append(thread)
         thread.start()
 
     for thread in threads:
         thread.join()
     print(f'Found {len(FOUND)} codes: {FOUND}')
-    return
 
 
 if __name__ == '__main__':
